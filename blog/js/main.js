65fd68d9a68524c65593df348701ce4decf5f613
Ricardo Villalobos <ricardovillalobos0990@gmail.com>
2019-12-10 12:08:11 -0500

    Inicio de proyecyo con Bootstrap - Js

--- blog/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const imgLightbox = document.querySelectorAll(".materialboxed");
-    M.Materialbox.init(imgLightbox, {
-    inDuration: 500,
-    outDuration: 500
-    });
-});
