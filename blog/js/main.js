8dc83c43dd1e38c1fff0d9f96d9f957ba9653981
Ricardo Villalobos <ricardovillalobos0990@gmail.com>
2019-12-12 15:47:36 -0500

    Organizando el modo celular con el icono de la X

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,13 +1,29 @@
+// ANIMACION PARA LOS TITULOS
 document.addEventListener("DOMContentLoaded", () => {
     const imgLightbox = document.querySelectorAll(".materialboxed");
     M.Materialbox.init(imgLightbox, {
-    inDuration: 500,
-    outDuration: 500
+        inDuration: 500,
+        outDuration: 500
     });
 });
-
-$(function(){
+//ANIMACION PARA LA BARRA DE MENU
+$(function () {
     var header = document.getElementById('header');
     var headroom = new Headroom(header);
     headroom.init();
 });
+
+const toggleNav = document.getElementById("toggleNav");
+toggleNav.addEventListener("click", cambiarIcono);
+
+var contador = 0;
+
+function cambiarIcono() {
+    if (contador == 0) {
+        toggleNav.innerHTML = "&#9747";
+        contador = 1;
+    } else {
+        toggleNav.innerHTML = "&#9777";
+        contador = 0;
+    }
+}
