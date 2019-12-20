e77f1d79303f69f8bc0ec7c844c3367e55ed91eb
Ricardo Villalobos <ricardovillalobos0990@gmail.com>
2019-12-19 23:52:51 -0500

    Probando miinificacion de archivos WEB PACK

--- blog/js/main.js
+++ blog/js/main.js
@@ -6,21 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
         outDuration: 500
     });
 });
-//ANIMACION PARA LA BARRA DE MENU
-$(function () {
-    var header = document.getElementById('header');
-    var headroom = new Headroom(header);
-    headroom.init();
-});
-const toggleNav = document.getElementById("toggleNav");
-toggleNav.addEventListener("click", cambiarIcono);
+const nav = document.getElementById("toggleNav");
+nav.addEventListener("click", cambiarIcono);
 var contador = 0;
 function cambiarIcono() {
     if (contador == 0) {
-        toggleNav.innerHTML = "&#9747";
+        nav.innerHTML = "&#9747";
         contador = 1;
     } else {
-        toggleNav.innerHTML = "&#9777";
+        nav.innerHTML = "&#9777";
         contador = 0;
     }
 }
